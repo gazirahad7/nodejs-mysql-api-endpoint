f487f3475d1d088c021a800f8fcb70f7e3776379
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-07-23 12:47:17 +0600

    Added

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
+const postsRouter = require("./routes/posts.router");
+const authRouter = require("./routes/auth.router");
+app.use(cors());
 
-app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/auth", authRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+  console.log("Server is running....");
+});
